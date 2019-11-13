bee180c4ed67532a73c3b752b4311ba65bba3c0e
Bent <bruntoe@outlook.dk>
2019-11-13 18:42:00 +0100

    Tilføjet pm2 konfigurationsfil

--- README.md
+++ README.md
@@ -8,9 +8,35 @@ Laravel anvendes ikke.
 
 - opdateret til Homestead ^9.0
 - anvender laravel/homested box 8.2.1
-- php ændret til 7.3
-- nodedemo.test er nu en proxy 
+- aktiveret pm2 
+- jsdemo.test er en proxy som anvendes af alle apps
 
+### pm2 konfigurationsfil
+
+- konfigurationsfilen ecosystem.config.js indehodler definition af alle apps til brug ved start inde fra Vagrant instansen.
+
+- husk kun at starte en app ad gangen på port 3000
+
+```
+    cd /vagrant
+    pm2 start ecosystem.config.js --only nodedemo
+```
+    
+når der skiftes så skal den aktive app standses
+
+```
+    pm2 stop nodedemo
+```
+
+Apps der anvender port 3000 er nodedemo, imgploadr, mongodemo, jstraining
+
+appen jsonserver kører på port 3300 og kan køre sammen med øvrie apps.
+
+monitering foregår med
+
+```
+    pm2 monit
+```
 
 ## Etablering
 Denne defintion er oprettet som en lokal instans ved med composer at installere **laravel/homestead** 
@@ -49,3 +75,5 @@ IMPORTANT Homebox apps med javascript node app
 - nu er der adgang til den app hvis node server er startet
 - afbryd node appen
 -  skift til en anden app og start denne med npm start
+
+## start 
---
+++ ecosystem.config.js
@@ -0,0 +1,60 @@
+/** filen er placeret i /vagrant
+ *
+ */
+module.exports = {
+    apps: [{
+        name: 'jstraining',
+        cwd: '/home/vagrant/jstraining/web',
+        script: '/home/vagrant/jstraining/web/bin/www',
+        instances: 1,
+        autorestart: true,
+        watch: true,
+
+    },
+        {
+            name: 'jsonserver',
+            cwd: '/home/vagrant/jstraining/web',
+            script: '/home/vagrant/jstraining/web/serverbookstore.js',
+            instances: 1,
+            autorestart: true,
+            watch: true,
+        },
+        {
+            name: 'imgploadr',
+            cwd: '/home/vagrant/imgploadr',
+            script: '/home/vagrant/imgploadr/server.js',
+            instances: 1,
+            autorestart: true,
+            watch: true,
+        },
+        {
+            name: 'nodedemo',
+            cwd: '/home/vagrant/nodedemo',
+            script: '/home/vagrant/nodedemo/bin/www.js',
+            instances: 1,
+            autorestart: true,
+            watch: true,
+        },
+        {
+            name: 'mongodemo',
+            cwd: '/home/vagrant/mongodemo',
+            script: '/home/vagrant/mongodemo/bin/www',
+            instances: 1,
+            autorestart: true,
+            watch: true,
+        }
+
+
+
+    ]
+};
+/**
+ env: {
+      DB_SERVER: 'localhost',
+      DB_PORT: '3306',
+      DB_NAME: 'mystore',
+      DB_USERNAME: 'athlon38',
+      DB_PASSWORD: 'trine-73-glf'
+    }
+
+ */
